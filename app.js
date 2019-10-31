758f69a05ec6d4a215d4423089311ef676937bef
LechCieslak <cieslak.lech@gmail.com>
2019-10-31 16:02:18 +0100

    some tests

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hejo!")
+let ter = new Date;
+
+boberFunc = () => {
+    console.log("1. div");
+}
+
+boberFunc2 = () => {
+    console.log(ter.getHours()+":"+ter.getMinutes()+":"+ter.getSeconds());
+console.log(ter.getTime())
+}
+
